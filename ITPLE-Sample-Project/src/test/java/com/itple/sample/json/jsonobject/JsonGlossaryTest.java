b8011efe725e411651db2386cc342947b11372d3
bomkun <bomkun@itple.com>
2015-01-27 12:42:44 +0900

    test branch merge test

--- ITPLE-Sample-Project/src/test/java/com/itple/sample/json/jsonobject/JsonGlossaryTest.java
+++ ITPLE-Sample-Project/src/test/java/com/itple/sample/json/jsonobject/JsonGlossaryTest.java
@@ -39,6 +39,8 @@ public class JsonGlossaryTest {
 		logger.debug("ffffa");
 		logger.debug("testsss");
 		logger.debug("test barnch rebase test");
+		
+		logger.debug("test branch merge test");
 	}
 
 	@Test
